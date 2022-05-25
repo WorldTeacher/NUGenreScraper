3b121d05a54f14c705c8fb6ed8d7664770393060
WorldTeacher <kirchneralexander020@gmail.com>
2022-05-25 22:00:00 +0200

    add real variables for end

--- Program/variables.py
+++ Program/variables.py
@@ -19,3 +19,14 @@ list=['Interc**rse','S*x', 'Weak to Strong','Petty Protagonist']
 listrepl=['First Time Intercourse','Intercourse','Sex', 'Rape','Slaves','Fellatio','Handjob','Masturbation','Anal','Prostitutes','Sex Slaves']
 censors=['First Time Interc**rse','Interc**rse','S*x','R*pe','S*aves','F*llatio','H*ndjob','M*sturbation','An*l','Prostit**es','S*x S*aves']
 replacelist={'old': ['First-time Interc**rse', 'Interc**rse', 'S*x', 'R*pe', 'S*aves', 'F*llatio', 'H*ndjob', 'M*sturbation', 'An*l', 'Prostit**es', 'S*x S*aves'], 'new': ['First-time Intercourse', 'Intercourse', 'Sex', 'Rape', 'Slaves', 'Fellatio', 'Handjob', 'Masturbation', 'Anal', 'Prostitutes', 'Sex Slaves']}
+
+'''
+Real Variables below this, to be implemented later on
+'''
+
+Calibre_Library_Path='/mnt/sd/Full Library/'
+#in Calibre_Library_Path, find all opf files
+
+Calibre_opf_files=glob.glob(Calibre_Library_Path+'**/metadata.opf', recursive=True)
+replacelist={'old': ['First-time Interc**rse', 'Interc**rse', 'S*x', 'R*pe', 'S*aves', 'F*llatio', 'H*ndjob', 'M*sturbation', 'An*l', 'Prostit**es', 'S*x S*aves'], 'new': ['First-time Intercourse', 'Intercourse', 'Sex', 'Rape', 'Slaves', 'Fellatio', 'Handjob', 'Masturbation', 'Anal', 'Prostitutes', 'Sex Slaves']}
+#If a new replacelist is needed, or if tags need to be added, run the replacechange.py script
