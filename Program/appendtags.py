9412f0617d787c38bce834af366719fbcf5f046d
WorldTeacher <kirchneralexander020@gmail.com>
2022-05-28 10:39:26 +0200

    update

--- Program/appendtags.py
+++ Program/appendtags.py
@@ -2,6 +2,7 @@ import variables
 import re
 from bs4 import BeautifulSoup
 import glob
+import json
 
 def append_tags():
     add_list=1
@@ -23,24 +24,37 @@ def append_tags():
             with open(i, 'w') as file:
                 file.write(soup.prettify())'''
     for i in variables.test:
-        with open(i,'r') as f:
-            tags=['test3','test4']
-            #find meta for tags and add content to it
-            soup=BeautifulSoup(f, 'lxml')
-            #print(soup)
-            for meta in soup.find_all('meta'):
-                if meta.get('name')=='calibre:user_metadata:#tags':
-                    dict={'val':[]}
-                    #split meta.get(name) into a list
-                    splitter=meta.get('content')[1:-1]
-                    #split splitter at the ':'
-                    print(splitter)
-                    '''splittered=splitter.split(':')
-                    #append variables.tags to splittered[1]
-                    for tag in tags:
-                        splittered[1]=splittered[1]+'"'tag
-                    print(splittered)
-'''
+        f=open(i,'r')
+        data=f.read()
+        f.close()
+        
+        tags=['test3','test4']
+        #find meta for tags and add content to it
+        soup=BeautifulSoup(data, 'lxml')
 
+        for meta in soup.find_all('meta'):
+            if meta.get('name')=='calibre:user_metadata:#tags':
+                metastr=str(meta)
+                
+                #dict={'val':[]}
+                #split meta.get(name) into a list
+                splitter=meta.get('content')
+                #print(meta.find('content'))
+                splitterjson=json.loads(splitter)
+                splitterjson['#value#']=tags
+                splitterstr=str(splitterjson)
+                #print(splitterstr)
+                splitterstr=splitterstr.replace("\'","\"")
+
+                #print(meta)
+        
+        #print(data.replace(metastr,splitterstr))
+        print(data.find(metastr))
+        #f=open(i,'w')
+       # f.write(data.replace(,splitterstr))
+        #f.close()
+
+        #f=open(i,'w')
+        #f.write()
 if __name__=='__main__':
     append_tags()
