ad2c7a8295ae5b8ed342da5d686506e612bed021
WorldTeacher <kirchneralexander020@gmail.com>
2022-07-21 22:19:19 +0200

    changed logger names

--- Program/logger.py
+++ Program/logger.py
@@ -23,29 +23,55 @@ class log:
         self.handler2.setFormatter(formatter2)
         # Add the handlers to the logger
         self.logger2.addHandler(self.handler2)
+        #create third logger for the general program
+        self.logger3 = logging.getLogger('General')
+        self.logger3.setLevel(logging.DEBUG)
+        # Create a handler
+        self.handler3 = logging.FileHandler('program_log.log')
+        self.handler3.setLevel(logging.DEBUG)
+        # Create a logging format
+        formatter3 = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.handler3.setFormatter(formatter3)
+        # Add the handlers to the logger
+        self.logger3.addHandler(self.handler3)
 
 
-    def info(self,message):
+
+    def info_database(self,message):
         self.logger.info(message)
-    def error(self,message):
+    def error_database(self,message):
         self.logger.error(message)
-    def debug(self,message):
+    def debug_database(self,message):
         self.logger.debug(message)
-    def warning(self,message):
+    def warning_database(self,message):
         self.logger.warning(message)
-    def critical(self,message):
+    def critical_database(self,message):
         self.logger.critical(message)
-    def fatal(self,message):
+    def fatal_database(self,message):
         self.logger.fatal(message)
-    def info2(self,message):
+    def info_webscraper(self,message):
         self.logger2.info(message)
-    def rror2(self,message):
+    def error_webscraper(self,message):
         self.logger2.error(message)
-    def debug2(self,message):
+    def debug_webscraper(self,message):
         self.logger2.debug(message)
-    def warning2(self,message):
+    def warning_webscraper(self,message):
         self.logger2.warning(message)
-    def critical2(self,message):
+    def critical_webscraper(self,message):
         self.logger2.critical(message)
-    def fatal2(self,message):
+    def fatal_webscraper(self,message):
         self.logger2.fatal(message)
+    def info_general(self,message):
+        self.logger3.info(message)
+    def error_general(self,message):
+        self.logger3.error(message)
+    def debug_general(self,message):
+        self.logger3.debug(message)
+    def warning_general(self,message):
+        self.logger3.warning(message)
+    def critical_general(self,message):
+        self.logger3.critical(message)
+    def fatal_general(self,message):
+        self.logger3.fatal(message)
+
+
